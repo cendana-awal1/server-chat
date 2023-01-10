6bfae1531fc39868b650b31670d020f45f51fa92
awal-cendana <108106499+cendana-awal1@users.noreply.github.com>
2023-01-10 19:37:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ const openai = new OpenAIApi(configration);
 // console.log(process.env.OPENAI_KEY);
 
 const app = express();
-app.use(cors({
-    origin: "*",
-  }));
+const corsOpt = {
+    origin: process.env.CORS_ALLOW_ORIGIN || '*', // this work well to configure origin url in the server
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
+    allowedHeaders: ['Content-Type', 'Authorization'] // allow json and token in the headers
+};
+app.use(cors(corsOpt));
 app.use(express.json());
 
 app.post("/", async (req, res) => {
